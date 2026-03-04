89047f044f155010360f0809f54806522521f605
alexthegoood <mail.alexthegoood@gmail.com>
2026-03-04 15:49:43 +0200

    init

---
+++ src/main/java/com/github/assemblyDir/keepInventory/KeepInventory.java
@@ -0,0 +1,18 @@
+package com.github.assemblyDir.keepInventory;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class KeepInventory extends JavaPlugin {
+
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+}
