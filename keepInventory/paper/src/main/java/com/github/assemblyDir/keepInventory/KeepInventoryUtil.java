d458fa39663fab1fe46caad0b4be50d6e91fc1fc
alexthegoood <mail.alexthegoood@gmail.com>
2026-03-07 15:12:59 +0200

    small KeepInventoryUtil improvements

--- keepInventory/paper/src/main/java/com/github/assemblyDir/keepInventory/KeepInventoryUtil.java
+++ keepInventory/paper/src/main/java/com/github/assemblyDir/keepInventory/KeepInventoryUtil.java
@@ -1,22 +1,20 @@
 package com.github.assemblyDir.keepInventory;
 
-import org.bukkit.NamespacedKey;
 import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.persistence.PersistentDataType;
 
 public final class KeepInventoryUtil {
 
-    private static final NamespacedKey key = KeepInventoryNamespacedKey.get();
-    private static final PersistentDataType<Byte, Boolean> dataType = PersistentDataType.BOOLEAN;
+    private static final PersistentDataType<Byte, Boolean> PERSISTENT_DATA_TYPE = PersistentDataType.BOOLEAN;
 
     public static boolean keepInventory(PersistentDataContainer persistentDataContainer) {
-        Boolean isEnabled = persistentDataContainer.get(key, dataType);
+        Boolean isEnabled = persistentDataContainer.get(KeepInventoryNamespacedKey.get(), PERSISTENT_DATA_TYPE);
         if (isEnabled != null) return isEnabled;
         else return false;
     }
 
     public static void keepInventory(PersistentDataContainer persistentDataContainer, Boolean isEnabled) {
-        persistentDataContainer.set(key, dataType, isEnabled);
+        persistentDataContainer.set(KeepInventoryNamespacedKey.get(), PERSISTENT_DATA_TYPE, isEnabled);
     }
 
 }
